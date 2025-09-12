c08f933b40b5a8aaa041500116d6468d03ab66fb
MiguelBenitez <miguelbenitezpikir@gmail.com>
2025-09-12 15:18:02 -0300

    Cambios minimos a los archivos base

--- accounts/models.py
+++ accounts/models.py
@@ -1,11 +1,9 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.utils import timezone
 
 class CustomUser(AbstractUser):
     # 'id', 'username', 'email', 'password' ya vienen por defecto
     email = models.EmailField(unique=True)  # asegura emails únicos
-    register_date = models.DateTimeField(default=timezone.now)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
