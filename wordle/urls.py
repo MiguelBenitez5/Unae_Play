830eaf3adac5c3e5098ebe374dc0941c34f0083f
MiguelBenitez <miguelbenitezpikir@gmail.com>
2025-09-20 12:30:15 -0300

    Mejorar al wordle del lado del servidor

--- wordle/urls.py
+++ wordle/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import render_page, play_wordle, reset_game
+from .views import render_page, play_wordle, reset_game, get_initial_data
 
 urlpatterns = [
     path('', render_page, name='wordle'),
     path('<str:userword>/',play_wordle ),
     path('action/restart/', reset_game),
+    path('action/getdata/', get_initial_data),
 ]
