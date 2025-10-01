8af5ba233d84f7f4f323af15be31d0efbc2fed51
MiguelBenitez <miguelbenitezpikir@gmail.com>
2025-10-01 16:38:28 -0300

    -Agregadas animaciones para la ventana modal al abrir y cerrar -Modificacion a la estructura del ranking modal para visualizar correctamente los datos de los juegos y ranking

--- globals/static/modal_scripts.js
+++ globals/static/modal_scripts.js
@@ -2,11 +2,21 @@ const modal = document.getElementById('rankingModal');
 const rankingFilter = document.getElementById('rankingFilter');
 const rankingTable = document.getElementById('rankingTable').querySelector('tbody');
 const closeBtn = document.querySelector('.close-btn')
+const modalContent = document.querySelector('.modal-content')
 
 
 // Cerrar modal
 closeBtn.addEventListener('click', () => {
-    modal.style.display = 'none';
+    modal.style.animation = 'none'
+    void modal.offsetWidth
+    modal.style.animation = 'myModalCloseAnim 1s ease-out 0s 1 normal forwards'                                                                                                                               
+    modal.addEventListener('animationend', function handler(e){
+        if(e.animationName === 'myModalCloseAnim'){
+            modal.style.display = 'none';
+            modal.removeEventListener('animationend', handler)
+        }
+    })
+    
 });
 
 // Cerrar modal haciendo click fuera del contenido
@@ -15,3 +25,9 @@ window.addEventListener('click', (e) => {
         modal.style.display = 'none';
     }
 });
+
+function showModalScreen(){
+    
+    modal.style.display = 'flex'
+}
+
