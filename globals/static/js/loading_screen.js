b29ff980e884eb905989546f583262c02ef6abc1
MiguelBenitez <miguelbenitezpikir@gmail.com>
2025-10-08 12:07:49 -0300

    Globals: -Fueron agregadas varias tipografias -Fueron agregadas 4 pantallas de carga que se muestran de forma aleatoria -La panatalla de carga es aprovechada para cargar los recursos disponibles -Fueron eliminados iconos y audios en desuso -Fueron agregados audios comprimidosm ideales para un sitio web -Los dialogos del personajes fueron ajustados para llevar la nueva tipografias y ser responsivos

--- globals/static/js/loading_screen.js
+++ globals/static/js/loading_screen.js
@@ -5,11 +5,13 @@ const selectGame = document.querySelector('#juegos')
 const loadingIcon = document.querySelector('.loading-icon')
 const loadingText = document.querySelector('.loading-text')
 const gameMusic = document.querySelector('audio')
+const loadingImg = document.querySelector('.loading-img')
 
 const successIcon = '<i class="fa-solid fa-check" style="color: #e6e9f0;"></i>'
 const loadIcon = '<i class="fa-solid fa-spinner fa-spin-pulse" style="color: #e6e9f0;">'
 const preloadedImages = {}
 const preloadedAudios = {}
+const preloadedLoadingImages = {}
 
 // precargar imagenes
 function preloadImage(path){
@@ -46,7 +48,7 @@ function preloadAudio(path){
 
 // carga de archivos
 async function preloadAll() {
-  const imagePaths = ['/static/img/buho.png', '/static/img/caballo.png',
+    const imagePaths = ['/static/img/buho.png', '/static/img/caballo.png',
                       '/static/img/camaleon.png', '/static/img/carpincho-1.png', 
                        '/static/img/COLONIAS_TALK.png','/static/img/conejo.png',
                        '/static/img/FACAT_TALK.png','/static/img/FACEM_TALK.png',
@@ -56,23 +58,36 @@ async function preloadAll() {
                        '/static/img/serpiente.png','/static/img/tucan.png',
                        '/static/img/imagenEjemplo1.jpg'
                     ]
-  const audioPaths = ['/static/music/adivinarnumero.mp3', '/static/music/ahorcado.wav',
-                      '/static/music/buscaminas.wav', '/static/music/landing_page.wav',
-                      '/static/music/memorygame.wav', '/static/music/piedrapapeltijera.wav',
-                      '/static/music/questions.mp3', '/static/music/tateti.wav',
-                      '/static/music/wordle.wav',
+    const audioPaths = ['/static/music/adivinarnumero.mp3', '/static/music/ahorcado.mp3',
+                      '/static/music/buscaminas.mp3', '/static/music/landing_page.mp3',
+                      '/static/music/memorygame.mp3', '/static/music/piedrapapeltijera.mp3',
+                      '/static/music/questions.mp3', '/static/music/tateti.mp3',
+                      '/static/music/wordle.mp3',
                     ]
 
-  const promises = [
-    ...imagePaths.map(preloadImage),
-    ...audioPaths.map(preloadAudio)
-  ]
+    const promises = [
+        ...imagePaths.map(preloadImage),
+        ...audioPaths.map(preloadAudio)
+    ]
 
-  await Promise.all(promises)
-  console.log('✅ Todos los recursos cargados')
+    await Promise.all(promises)
+    loadingText.textContent = 'Juego completamente cargado'
+    loadingIcon.innerHTML = successIcon
+    loadingBtn.style.display = 'inline'
+    console.log('✅ Todos los recursos cargados')
 }
 
-preloadAll()
+async function loadingImagesPreload() {
+    const imagePaths = [ '/static/img/loading1.png', '/static/img/loading2.jpeg',
+                         '/static/img/loading3.jpg','/static/img/loading4.jpg'
+                       ]
+    const promises = [...imagePaths.map(preloadImage)]
+
+    await Promise.all(promises)
+    // seleccion de una imagen aleatoria
+    loadingImg.style.display = 'inline'
+    loadingImg.src = imagePaths[Math.floor(Math.random()*imagePaths.length)]
+}
 
 // se muestra la pantalla de carga por 5 segundos
 function showLoadingScreen(){
@@ -81,14 +96,13 @@ function showLoadingScreen(){
     loadingText.textContent = 'Cargando juego'
     loadingIcon.innerHTML = loadIcon
     loadingBtn.style.display = 'none'
-    setTimeout(()=>{
-        loadingText.textContent = 'Juego completamente cargado'
-        loadingIcon.innerHTML = successIcon
-        loadingBtn.style.display = 'inline'
-    }, 5000)
+    preloadAll()
 }
 
-window.onload = showLoadingScreen
+window.onload = ()=>{
+    loadingImagesPreload()
+    showLoadingScreen()
+}
 
 // evento para el boton de juego cargado
 loadingBtn.addEventListener('click', ()=>{
@@ -96,3 +110,4 @@ loadingBtn.addEventListener('click', ()=>{
     loadingScreen.style.display = 'none'
     gameScreen.style.display = 'block'
 })
+
