c05d1b68815eec1ce5d0957224c7f479ec2ab153
Nahuel-09 <nl618275@gmail.com>
2025-10-08 16:06:19 -0300

    featuring de limpieza de tabla en router

--- buscaminas/urls.py
+++ buscaminas/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.game_page, name='buscaminas_game'),
     path('start/', views.start_game, name='start_game'),
     path('reveal/<int:row>/<int:col>/', views.reveal_cell, name='reveal_cell'),
+    path('restart/', views.restart_game, name='restart_game'),
 ]
