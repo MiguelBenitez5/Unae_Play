309053a05034ca3c24e714711112ab15fd8b149a
MiguelBenitez5 <miguelbenitezpikir@gmail.com>
2025-10-08 19:07:16 -0300

    Update settings.py

--- minigames/settings.py
+++ minigames/settings.py
@@ -88,24 +88,24 @@ WSGI_APPLICATION = 'minigames.wsgi.app'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'minigames_project',                          
-        'USER': 'postgres',                        
-        'PASSWORD': '1234567',                   
-        'HOST': 'localhost',                          
-        'PORT': '5432',   
-    }
-}
-
 # DATABASES = {
-#     'default': dj_database_url.config(
-#         default=os.environ.get('DATABASE_URL'),
-#         conn_max_age=600,
-#         ssl_require=True 
-#     )
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'minigames_project',                          
+#         'USER': 'postgres',                        
+#         'PASSWORD': '1234567',                   
+#         'HOST': 'localhost',                          
+#         'PORT': '5432',   
+#     }
 # }
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True 
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
@@ -148,11 +148,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-# Directorios donde Django busca archivos estáticos durante desarrollo
-STATICFILES_DIRS = [
-    BASE_DIR / 'globals' / 'static',
-]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
