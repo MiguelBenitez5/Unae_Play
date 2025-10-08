a97e6504461c2ba7f14a905b03c6bb2b9b1615d4
MiguelBenitez5 <miguelbenitezpikir@gmail.com>
2025-10-08 18:40:32 -0300

    Update settings.py

--- minigames/settings.py
+++ minigames/settings.py
@@ -28,7 +28,7 @@ load_dotenv(BASE_DIR / '.env')
 SECRET_KEY = 'django-insecure-o4*r76diz!m%jf0gzoz-jm7-_^rzakkjik_u1vqvmnt^w2aaqh_adasd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['unae-play.onrender.com','.vercel.app','localhost','127.0.0.1']
 
@@ -88,24 +88,24 @@ WSGI_APPLICATION = 'minigames.wsgi.app'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'minigames_project',                          
-        'USER': 'postgres',                        
-        'PASSWORD': '1234567',                   
-        'HOST': 'localhost',                          
-        'PORT': '5432',   
-    }
-}
-
 # DATABASES = {
-#     'default': dj_database_url.config(
-#         default=os.environ.get('DATABASE_URL'),
-#         conn_max_age=600,
-#         ssl_require=True 
-#     )
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'minigames_project',                          
+#         'USER': 'postgres',                        
+#         'PASSWORD': '1234567',                   
+#         'HOST': 'localhost',                          
+#         'PORT': '5432',   
+#     }
 # }
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True 
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
