2218e5904cd878565336dad74072c2399a44fa84
MiguelBenitez <miguelbenitezpikir@gmail.com>
2025-10-30 11:01:52 -0300

    a

--- minigames/settings.py
+++ minigames/settings.py
@@ -134,10 +134,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = '/var/www/Unae_play/static/'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = '/var/www/Unae_play/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
